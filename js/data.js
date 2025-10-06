0558fb53bcef3e3c4765565b3343e70df7230c93
Vasilije Bešović <vasilije.besovic@coreit.me>
2025-10-06 10:04:44 +0200

    Scoreboard page and logic.

--- js/data.js
+++ js/data.js
@@ -12,6 +12,12 @@ let cpuShips = 0;
 
 let gameType = "pvp";
 
+let player1Wins = 0;
+
+let player2Wins = 0;
+
+let cpuWins =  0;
+
 let player1WinRatio = 0;
 
 let player2WinRatio = 0;
@@ -19,3 +25,5 @@ let player2WinRatio = 0;
 let cpuWinRatio = 0;
 
 let gamesPlayed = 0;
+
+let games = [{winner: '', startTime: '', type: '', player1Ships: '', player2Ships: '', cpuShips: ''}]
---
+++ js/scoreboard.js
@@ -0,0 +1,44 @@
+let simpleStats = document.querySelectorAll(".simple-stat");
+let detailedStats = document.querySelector(".detailed-stats");
+
+simpleStats.forEach((stat) => {
+    switch(stat.id) {
+        case 'player1-wins':
+            stat.innerHTML += " " + player1Wins;
+            break;
+        case 'player2-wins':
+            stat.innerHTML += " " + player2Ships;
+            break;
+        case 'cpu-wins':
+            stat.innerHTML += " " + cpuWins;
+            break;
+        case 'player1-win-ratio':
+            stat.innerHTML += " " + player1WinRatio;
+            break;
+        case 'player2-win-ratio':
+            stat.innerHTML += " " + player2WinRatio;
+            break;
+        case 'cpu-win-ratio':
+            stat.innerHTML += " " + cpuWinRatio;
+            break;
+        case 'games-played':
+            stat.innerHTML += " " + gamesPlayed;
+            break;
+    }
+});
+
+let gameNum = 1;
+
+games.forEach((game) => {
+    detailedStats.innerHTML += "" +
+        "<div class=\"row\">\n" +
+        "        <div class=\"column\">"+ gameNum + "</div>\n" +
+        "        <div class=\"column\">" + game.winner +"</div>\n" +
+        "        <div class=\"column\">" + game.type + " </div>\n" +
+        "        <div class=\"column\">" + game.player1Ships + " </div>\n" +
+        "        <div class=\"column\">" + game.player2Ships +"</div>\n" +
+        "        <div class=\"column\">" + game.cpuShips + "</div>\n" +
+        "</div>";
+    gameNum++;
+});
+
